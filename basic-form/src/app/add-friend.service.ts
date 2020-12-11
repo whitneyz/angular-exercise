4de59b4537cc9d445e9a281642ee441b3d04711e
Whitneyz <66369610+whitneyz@users.noreply.github.com>
2020-12-11 14:27:49 +0100

    observable

--- basic-form/src/app/add-friend.service.ts
+++ basic-form/src/app/add-friend.service.ts
@@ -1,10 +1,19 @@
-import { Injectable } from '@angular/core';
-import {HttpClientModule} from "@angular/common/http";
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Friend} from './friend';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AddFriendService {
+  /*post request to fetch http*/
+  constructor(private http: HttpClient) {
+  }
+  url = '';
 
-  constructor() { }
+  /*parameter die binnenkomt moet een typehint zijn en daarnaast Observable is ook een typehint*/
+  addFriend(friend: Friend): Observable<any> {
+    return this.http.post(this.url, friend);
+  }
 }
--- basic-form/src/app/app.component.ts
+++ basic-form/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-
+import {AddFriendService} from './add-friend.service';
 import {Friend} from './friend';
 
 @Component({
@@ -10,10 +10,18 @@ import {Friend} from './friend';
 export class AppComponent {
   languages = ['html', 'css', 'js', 'php'];
   friendModel = new Friend('', '', '', 0, []);
-  public showFriend(): any
-  {
+
+  constructor(private addFriendService: AddFriendService) {
+  }
+  public showFriend(): any {
+    this.addFriendService.addFriend(this.friendModel)
+    .subscribe(
+      succes => console.log(this.showFriend),
+      error => console.log(error)
+    );
     console.log(this.friendModel);
   }
+
 
 }
 
